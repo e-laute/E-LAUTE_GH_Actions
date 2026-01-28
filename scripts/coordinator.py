0fd1577890a5f6f20f9e573d56cfb8d1eb92ef64
Brathenning <125910965+Brathenning@users.noreply.github.com>
2026-01-28 18:51:19 +0100

    added importlib to coordinator

--- scripts/coordinator.py
+++ scripts/coordinator.py
@@ -1,13 +1,13 @@
+import importlib
 import json
 from lxml import etree
-import script_collection
 
 def setup(filepath:str, workpatch:str, **addargs):
 
     with open("workpatches.json") as f:
         workpatches_dic = json.load(f)
     try:
-        sripts_li = workpatches_dic[workpatch]["scripts"]
+        scripts_li = workpatches_dic[workpatch]["scripts"]
     except KeyError:
         raise Exception("Unknown or faulty workpatch")
     
@@ -17,8 +17,20 @@ def setup(filepath:str, workpatch:str, **addargs):
 
     if workpatches_dic[workpatch]["sibling"]:
         sibling_root = getsibling(filepath)
+    else:
+        sibling_root = None
 
-    
-    for script in sripts_li:
-        current_func = getattr(script_collection,script)
-        root = current_func(root,sibling_root,**addargs)
+    #scripts in the JSON is a list of module to function paths (dir.subdir.module.func)
+    #modules_dic contains the path of the module as key (dir.subdir.module) and the loaded module as item
+    modules_li = list(set([script.rpartition(".")[0] for script in scripts_li]))
+    try:
+        modules_dic = {mod:importlib.import_module(mod) for mod in modules_li}
+    except ImportError:
+        raise Exception("Unknown module")
+
+    for script in scripts_li:
+        module_path, dot, func_name = script.rpartition(".")
+        current_func = getattr(modules_dic[module_path],func_name,None)
+        if current_func is None:
+            raise Exception(f"Unknown script or wrong module path: {script}")
+        root = current_func(root,sibling_root,addargs)
