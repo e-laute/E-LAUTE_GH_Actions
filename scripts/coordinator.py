afacfeaa30d6e0ba511b58ff2327d0a1b0a6fe04
Julia Jaklin <julia.jaklin@gmail.com>
2026-01-29 15:04:34 +0100

    Reformat with Prettier and rename occurrences of 'workpatch' with 'workpackage'

--- scripts/coordinator.py
+++ scripts/coordinator.py
@@ -1,10 +1,7 @@
 """
-coordinates the workpatch with the associated file(s)
+Coordinates the workpackage with the associated file(s)
 """
 
-import os
-import re
-import copy
 import sys
 from pathlib import Path
 import argparse
@@ -12,113 +9,132 @@ import importlib
 import json
 from lxml import etree
 
-#initializing
-parser =argparse.ArgumentParser(description="Coordinates the execution of scripts in the workpatch on filepath")
+# initializing
+parser = argparse.ArgumentParser(
+    description="Coordinates the execution of scripts in the workpackage on filepath"
+)
 include = parser.add_mutually_exclusive_group(required=True)
-include.add_argument("-i","--include",nargs="*", help="Included files by id number")
-include.add_argument("-f","--filepath",help="A specific filepath")
-parser.add_argument("-w","--workpatch",required=True, help="The workpatch to be executed")
-parser.add_argument("-a","--addargs",nargs="*", help="Additional arguments required by the workpatch, formatted key=value")
+include.add_argument(
+    "-i", "--include", nargs="*", help="Included files by id number"
+)
+include.add_argument("-f", "--filepath", help="A specific filepath")
+parser.add_argument(
+    "-w", "--workpackage", required=True, help="The workpackage to be executed"
+)
+parser.add_argument(
+    "-a",
+    "--addargs",
+    nargs="*",
+    help="Additional arguments required by the workpackage, formatted key=value",
+)
 
-with open("workpatches.json") as f:
-    workpatches_dic = json.load(f)
+with open("workpackages.json") as f:
+    workpackages_dic = json.load(f)
 
-def execute_workpatch(filepath:str, workpatch:dict, addargs:dict):
+
+def execute_workpackage(filepath: str, workpackage: dict, addargs: dict):
     """
-    Parses filepath, loads the specefied workpatch from workpath 
+    Parses filepath, loads the specefied workpackage from workpath
     and calls the designated scripts on the parsed file.
-    
+
     :param filepath: the file to be processed
     :type filepath: str
-    :param workpatch: the workpatch to be executed
-    :type workpatch: dict
-    :param addargs: arguments required for the workpatch
+    :param workpackage: the workpackage to be executed
+    :type workpackage: dict
+    :param addargs: arguments required for the workpackage
     """
     try:
-        scripts_li = workpatch["scripts"]
+        scripts_li = workpackage["scripts"]
     except KeyError:
-        raise Exception("Faulty workpatch, missing 'scripts'")
-    
+        raise Exception("Faulty workpackage, missing 'scripts'")
+
     with open(filepath) as f:
-        tree = etree.parse(f,etree.XMLParser(recover=True))
+        tree = etree.parse(f, etree.XMLParser(recover=True))
     root = tree.getroot()
 
-    #TODO differentiate sibling type
-    if workpatch["sibling"]:
+    # TODO differentiate sibling type
+    if workpackage["sibling"]:
         sibling_root = getsibling(filepath)
     else:
         sibling_root = None
 
-    #scripts in the JSON is a list of module to function paths (dir.subdir.module.func)
-    #modules_dic contains the path of the module as key (dir.subdir.module) and the loaded module as item
+    # scripts in the JSON is a list of module to function paths (dir.subdir.module.func)
+    # modules_dic contains the path of the module as key (dir.subdir.module) and the loaded module as item
     modules_li = list(set([script.rpartition(".")[0] for script in scripts_li]))
     try:
-        modules_dic = {mod:importlib.import_module(mod) for mod in modules_li}
+        modules_dic = {mod: importlib.import_module(mod) for mod in modules_li}
     except ImportError:
         raise Exception("Unknown module")
 
     for script in scripts_li:
         module_path, dot, func_name = script.rpartition(".")
-        current_func = getattr(modules_dic[module_path],func_name,None)
+        current_func = getattr(modules_dic[module_path], func_name, None)
         if current_func is None:
             raise Exception(f"Unknown script or wrong module path: {script}")
-        #TODO currently scripts need to handle all input
-        root = current_func(root,sibling_root,addargs)
+        # TODO currently scripts need to handle all input
+        root = current_func(root, sibling_root, addargs)
+
 
 def getsibling(filepath):
     pass
 
+
 def main():
     """
-    Parses Arguments, selects file, calls coordinator on files with workpatch 
+    Parses Arguments, selects file, calls coordinator on files with workpackage
     """
-    #TODO needs to be rewritten to fit new design
-    #TODO needs to handle arguments passed, maybe argparse? 
-    #For now assumes python coordinator.py filepath workpatch additional arguments
-    #TODO needs to be able to handle multiple files
-    #TODO check for validity of workpatch x filetype, multiple files
+    # TODO needs to be rewritten to fit new design
+    # TODO needs to handle arguments passed, maybe argparse?
+    # For now assumes python coordinator.py filepath workpackage additional arguments
+    # TODO needs to be able to handle multiple files
+    # TODO check for validity of workpackage x filetype, multiple files
     args = parser.parse_args()
-    
 
     try:
-        workpatch = workpatches_dic[args.workpatch] #argv needs to contain workpatch
+        workpackage = workpackages_dic[
+            args.workpackage
+        ]  # argv needs to contain workpackage
     except KeyError:
-        print(f"::error::Unknown workpatch: {args.workpatch}")
+        print(f"::error::Unknown workpackage: {args.workpackage}")
         return 1
 
-    
     files = []
     if args.filepath:
         files.append(args.file)
     else:
         files = get_file_from_num(args.include)
-    
+
     for filepath in files:
         # hardcode 'caller-repo/' prefix to refer to caller (source) repository
-        mei_path = Path('caller-repo') / Path(filepath)
+        mei_path = Path("caller-repo") / Path(filepath)
         print(f"Checking file: {mei_path}")
         if not mei_path.is_file():
             print(f"::error::File not found: '{mei_path}'")
             return 2
-        
+
         try:
-            execute_workpatch(mei_path,workpatch,addargs_to_dic(args.addargs))
+            execute_workpackage(
+                mei_path, workpackage, addargs_to_dic(args.addargs)
+            )
             print("::notice::Process completed successfully")
             return 0
         except Exception as e:
             print(f"::error::Failed to process file: {e}")
             return 1
-    
+
+
 def get_file_from_num(*args):
     pass
 
-def addargs_to_dic(addargs:list):
+
+def addargs_to_dic(addargs: list):
     kwargs = {}
     for item in addargs:
-        if '=' in item:
-            key, value = item.split('=', 1) # Split only on the first '='
+        if "=" in item:
+            key, value = item.split("=", 1)  # Split only on the first '='
             kwargs[key] = value
     return kwargs
 
+
 if __name__ == "__main__":
-    sys.exit(main()) 
+    sys.exit(main())
