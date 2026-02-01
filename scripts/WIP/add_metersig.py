e3ca372410c529f07d334714d0bfd1fd2b7274bc
Brathenning <125910965+Brathenning@users.noreply.github.com>
2026-02-01 14:12:15 +0100

    updated add_metersig to lxml

--- scripts/WIP/add_metersig.py
+++ scripts/WIP/add_metersig.py
@@ -1,118 +1,61 @@
+import copy
 import os
 import re
-import xml.etree.ElementTree as ET
-import copy
 import sys
 
+from lxml import etree
 
-ns = {"":"http://www.music-encoding.org/ns/mei",
-        "xml":"http://www.w3.org/XML/1998/namespace"}
-
-def measure_lr(measure:ET.Element,left:bool,skip=False):
-    print(measure.attrib)
-    ret = "left" in measure.attrib and measure.attrib["left"]=="invis"
-    if skip:
-        annot = ET.SubElement(measure,"{http://www.music-encoding.org/ns/mei}annot",{"plist":f"#{measure.get("{http://www.w3.org/XML/1998/namespace}id")}"})
-        annot.text="previous measures in choice differed in @left"
-        measure.attrib.pop("left",None)
-        return ret
-    if left:
-        if ("right" in measure.attrib and measure.attrib["right"]!="invis"):
-            annot = ET.SubElement(measure,"{http://www.music-encoding.org/ns/mei}annot",{"plist":f"#{measure.get("{http://www.w3.org/XML/1998/namespace}id")}"})
-            annot.text="right should be invis, had other value"
-        else:
-            measure.set("right","invis")
-    measure.attrib.pop("left",None)
-    return ret
-
-def handle_choice(choice:ET.Element,left:bool):
-    left_orig = left_reg = left
-    try:
-        for elem in choice.find("orig",ns)[::-1]:
-                if elem.tag=="{http://www.music-encoding.org/ns/mei}measure":
-                    left_orig=measure_lr(elem,left_orig)
-        for elem in choice.find("reg",ns)[::-1]:
-                if elem.tag=="{http://www.music-encoding.org/ns/mei}measure":
-                    left_reg=measure_lr(elem,left_reg)
-    except TypeError:
-        return left
-    print(left_orig,left_reg)
-    if left_reg==left_orig:
-        return left_reg
-    return None
-    
+ns = {
+    "mei": "http://www.music-encoding.org/ns/mei",
+    "xml": "http://www.w3.org/XML/1998/namespace",
+}
 
 
+def update_ed_CMN_staffdef(file: str):
+    tree = etree.parse(file)
 
-def change_lr(file:str):
+    root = tree.getroot()
 
-    ET.register_namespace("","http://www.music-encoding.org/ns/mei")
+    meterSig = root.find(".//mei:meterSig", namespace=ns)
 
-    doc = ET.parse(file)
-
-    root = doc.getroot()
-
-    
-    meterSig = root.find(".//meterSig", ns)
-
-    mensur = root.find(".//mensur", ns)
+    mensur = root.find(".//mei:mensur", namespace=ns)
 
     if mensur is not None and meterSig is None:
-        staffdefs = root.findall(".//staffDef", ns)
+        staffdefs = root.findall(".//mei:staffDef", namespace=ns)
         if len(staffdefs) < 2:
             print(f"not enough staffdef for {file}")
             return
-        meterSig = ET.Element("meterSig")
+        meterSig = etree.Element("meterSig")
         if mensur.get("num") is not None:
-            meterSig.set("count","3")
+            meterSig.set("count", "3")
         elif mensur.get("slash") is not None and mensur.get("slash") == "1":
-            meterSig.set("count","4")
+            meterSig.set("count", "4")
         else:
-            meterSig.set("count","2")
-        meterSig.set("unit","4")
-        meterSig.set("enclose","brack")
+            meterSig.set("count", "2")
+        meterSig.set("unit", "4")
+        meterSig.set("enclose", "brack")
 
         staffdefs[0].append(meterSig)
         staffdefs[1].append(meterSig)
 
-    mei = ""
-    with open(file, "r", encoding="utf-8") as f:
-        while True:
-            line = f.readline()
-            if line[:2] != "<?":
-                break
-            mei += line
-        ET.indent(root, "   ")
-        mei += ET.tostring(root, "unicode")
-    """
-    left=False
-    skip_next=False
-    for section in sections[::-1]:
-        for elem in section[::-1]:
-            print(elem.tag)
-            if elem.tag=="{http://www.music-encoding.org/ns/mei}measure":
-                left=measure_lr(elem,left,skip_next)
-                skip_next=False
-            if elem.tag=="{http://www.music-encoding.org/ns/mei}choice":
-                left=handle_choice(elem,left)
-                skip_next=left==None
-            
-        
-"""
+    etree.indent(tree, "   ")
 
-    with open(file, "w", encoding="utf-8") as f:
-        f.write(mei)
+    # Write back, preserving XML declaration and processing instructions
+    with open(file, "wb") as f:
+        tree.write(f, encoding="UTF-8", pretty_print=True, xml_declaration=True)
 
-def changefilename():
+
+def choosefile():
     dir_path = os.path.abspath(os.path.join(os.path.dirname(__file__)))
     for root, dirs, files in os.walk(dir_path):
         for file in files:
-            if re.fullmatch(r"Jud.*_enc_ed_CMN\.mei",file)!=None:
-                if re.match(r"_n1[0-6]",file) is not None or "n25" in file:
-                    print(os.path.join(root,file))
+            if re.fullmatch(r"Jud.*_enc_ed_CMN\.mei", file) != None:
+                if re.match(r"_n1[0-6]", file) is not None or "n25" in file:
+                    print(os.path.join(root, file))
                     continue
-                #if re.sub(r"(.*)_enc_ed_CMN(\.mei)",r"\1_enc_ed_GLT\2",file) in files:
-                    #print("\nsucces\n",os.path.join(root,re.sub(r"(.*)_enc_ed_CMN(\.mei)",r"\1_enc_ed_GLT\2",file)))
-                change_lr(os.path.join(root,file))
+                # if re.sub(r"(.*)_enc_ed_CMN(\.mei)",r"\1_enc_ed_GLT\2",file) in files:
+                # print("\nsucces\n",os.path.join(root,re.sub(r"(.*)_enc_ed_CMN(\.mei)",r"\1_enc_ed_GLT\2",file)))
+                update_ed_CMN_staffdef(os.path.join(root, file))
 
-changefilename()
+
+choosefile()
