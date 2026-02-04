867acb1402180de8adb090e400ae8036d98839ad
Brathenning <125910965+Brathenning@users.noreply.github.com>
2026-02-04 12:45:41 +0100

    added add_GLT_from_header

--- scripts/script_collection.py
+++ scripts/script_collection.py
@@ -14,35 +14,6 @@ ns = {
 }
 
 
-def write_output(
-    active_dom: dict, context_doms: list, output_path: Path = None, **addargs
-):
-    # TODO tree.write needs tree not root. Either all scripts do tree.getroot or
-    # write output gets treated differenly
-    """
-    writes active_dom to output_path
-
-    :param active_dom: dict containing {filename:Path/str?, notationtype:str, dom:etree.Element}
-    :type active_dom: dict
-    :param context_doms: list containing dom dicts
-    :type context_doms: list
-    :param output_path: path of file to write DOM
-    :type output_path: Path
-    :param addargs: Addional arguments that are unused
-    """
-
-    root = active_dom["dom"]
-
-    # etree.indent(tree, "   ")
-
-    # Write back, preserving XML declaration and processing instructions
-    # with open(file, "wb") as f:
-    # tree.write(f, encoding="UTF-8", pretty_print=True, xml_declaration=True)
-
-    active_dom["dom"] = root
-    return active_dom
-
-
 def add_sbs_every_n(active_dom: dict, context_doms: list, sbInterval: int, **addargs):
     """
     Adds `<sb>` every n measures
@@ -96,7 +67,7 @@ def remove_all_sbs(active_dom: dict, context_doms: list, **addargs):
     return active_dom
 
 
-def function(active_dom: dict, context_doms: list, **addargs):
+def _template_function(active_dom: dict, context_doms: list, **addargs):
     """
     template function
 
@@ -110,6 +81,83 @@ def function(active_dom: dict, context_doms: list, **addargs):
     root = active_dom["dom"]
 
     xpath_result = root.xpath(".//mei:elem[@attrib='value']", namespaces=ns)
+
+    active_dom["dom"] = root
+    return active_dom
+
+
+def add_header_from_GLT(
+    active_dom: dict, context_doms: list, projectstaff: str, role: str, **addargs
+):
+    """
+    template function
+
+    :param active_dom: dict containing {filename:str, notationtype:str, dom:etree.Element}
+    :type active_dom: dict
+    :param context_doms: list containing dom dicts
+    :type context_doms: list
+    :param addargs: Addional arguments that are unused
+    """
+
+    root = active_dom["dom"]
+    for context_dom in context_doms:
+        if context_dom["notationtype"] == "ed_GLT":
+            helproot = context_dom["dom"]
+            break
+    else:
+        raise ValueError("add_header_from_GLT needs context_dom ed_GLT, not given")
+
+    if root.xpath(
+        ".//mei:corpName//mei:expan[text()='Electronic Linked Annotated Unified Tablature Edition']",
+        namespaces=ns,
+    ):
+        print(f"{active_dom["filename"]} already has header")
+        return active_dom
+
+    if not helproot.xpath(
+        ".//mei:corpName//mei:expan[text()='Electronic Linked Annotated Unified Tablature Edition']",
+        namespaces=ns,
+    ):
+        print(f"{active_dom["filename"]} has no header")
+        return active_dom
+
+    appInfo = root.find(".//mei:appInfo", namespaces=ns)
+
+    header = copy.deepcopy(helproot.find(".//mei:meiHead", namespaces=ns))
+
+    titlePart = header.find(".//mei:titlePart/mei:abbr", namespaces=ns)
+    titlePart.clear()
+    titlePart.set("expan", "Common Music Notation")
+    titlePart.text = "CMN"
+
+    edition = header.find(".//mei:edition", namespaces=ns)
+    edition.set("resp", projectstaff)
+    edition.text = f"First {'diplomatic transcription' if 'dipl' in active_dom["notationtype"] else 'edition'} in CMN. Lute tuned in A."
+
+    judenkunig = header.xpath(".//mei:persName[@corresp='#persons-78']", namespaces=ns)
+    if judenkunig:
+        judenkunig[0].set("role", role)
+
+    appinfoold = header.find(".//mei:appInfo", namespaces=ns)
+    encodingDesc = appinfoold.getparent()
+    encodingDesc.remove(appinfoold)
+    encodingDesc.insert(0, appInfo)
+
+    edps = header.xpath(".//mei:editorialDecl//p", namespaces=ns)
+
+    for edp in edps:
+        edp.text = ""
+
+    revisionDesc = header.find("./mei:revisionDesc", namespaces=ns)
+    del revisionDesc[1:]
+    revisionDesc[0].attrib.update({"isodate": "YYYY-MM-DD", "n": "1", "resp": "#"})
+    revps = revisionDesc.xpath(".//mei:p", namespaces=ns)
+
+    for revp in revps:
+        revp.text = ""
+
+    root.remove(root.find("./mei:meiHead", namespaces=ns))
+    root.insert(0, header)
 
     active_dom["dom"] = root
     return active_dom
