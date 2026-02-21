0205828169060a2b880b51d6379cf3ae6666c715
Brathenning <125910965+Brathenning@users.noreply.github.com>
2026-02-21 01:44:09 +0100

    prepared new funtion

--- scripts/WIP/addpb_orsectionfacs.py
+++ scripts/WIP/addpb_orsectionfacs.py
@@ -62,6 +62,123 @@ def manual_unwrap(element):
     parent.remove(element)
 
 
+def add_section_foldir_from_dipl_GLT_to_ed(
+    active_dom: dict, context_doms: list, **addargs
+):
+    """
+    Removes expansion and section containing sections, adds foldir and sections/pbs based on dipl_GLT
+
+    :param active_dom: dict containing {filename:Path/str?, notationtype:str, dom:etree.Element}
+    :type active_dom: dict
+    :param context_doms: list containing dom dicts
+    :type context_doms: list
+    :param addargs: Addional arguments that are unused
+    """
+    output_message = ""
+
+    if "ed" not in active_dom["filetype"]:
+        raise RuntimeError(f"{active_dom['filename']} must be ed_GLT or ed_CMN")
+
+    root = active_dom["dom"]
+    root: etree.Element = active_dom["dom"]
+    for context_dom in context_doms:
+        if context_dom["notationtype"] == "dipl_GLT":
+            help_dom = context_dom
+            helproot = help_dom["dom"]
+            break
+    else:
+        raise RuntimeError(
+            "add_section_foldir_from_dipl_GLT_to_ed needs context_dom dipl_GLT, not given"
+        )
+
+    if "ed" in active_dom["filetype"]:
+        section_in_section = root.xpath(".//mei:section//mei:section", namespaces=ns)
+
+        expansion = root.find(".//mei:expansion", namespaces=ns)
+        if expansion is not None:
+            expansion.getparent().remove(expansion)
+            for s in section_in_section:
+                manual_unwrap(s)
+        elif section_in_section:
+            RuntimeError(
+                f"{active_dom["filename"]} contains recursive section but no expansion"
+            )
+
+    section = root.find(".//mei:section", namespaces=ns)
+
+    if getmnum(root) == getmnum(helproot):
+        help_sections = helproot.xpath("//mei:section[@n]", namespaces=ns)
+    else:
+        raise RuntimeError(
+            f"{active_dom["filename"]} has diffrent number of measures from {help_dom["filename"]}"
+        )
+    # TODO implement getting info from dipl_GLT!
+    section_info = []
+    for help_section in help_sections:
+        help_measures = help_section.xpath(".//mei:measure", namespaces=ns)
+        foldir = help_measures[0].xpath("./mei:dir[@type='ref']", namespaces=ns)
+        if not foldir:
+            raise RuntimeError(f"no foldir found in {help_dom["filename"]}")
+        tstamp = foldir[0].get("tstamp")
+        section_info.append((help_measures[0].get("n"), help_section.get("n"), tstamp))
+
+    surfaces = root.xpath("//mei:facsimile/mei:surface", namespaces=ns)
+    if len(surfaces) != len(section_info):
+        raise RuntimeError(
+            f"surfaces don't match number of sections in {active_dom["filename"]}"
+        )
+
+    surface_id = [f"#{s.get(f'{{{ns['xml']}}}id')}" for s in surfaces]
+
+    score = section.getparent()
+
+    section_info = [
+        (
+            mnum,
+            fol,
+            tstamp,
+            etree.SubElement(score, "section", {"n": fol, "facs": facs}),
+        )
+        for (mnum, fol, tstamp), facs in zip(section_info, surface_id)
+    ]
+
+    section_children = list(section)
+    current_n = ""
+    current_section = 0
+    for child in section_children:
+        if child.tag == f"{{{ns['mei']}}}measure":
+            current_n = child.get("n", "")
+            if (
+                current_section != len(section_info) - 1
+                and current_n == section_info[current_section + 1][0]
+            ):
+                current_section += 1
+                section_info[current_section][3].append(child)
+                add_foldir(
+                    child,
+                    section_info[current_section][1],
+                    section_info[current_section][2],
+                )
+            else:
+                section_info[current_section][3].append(child)
+        else:
+            section_info[current_section][3].append(child)
+
+    if current_section != len(section_info) - 1:
+        raise RuntimeError(
+            f"{active_dom["filename"]} wasn't processed because of misalignment of section"
+        )
+
+    print("succes!!")
+
+    score.remove(section)
+
+    xpath_result = root.xpath(".//mei:elem[@attrib='value']", namespaces=ns)
+
+    active_dom["dom"] = root
+    return active_dom, output_message
+
+
 def add_facs_and_foldir(file: str, helpfile: str):
     with open(file, "rb") as h:
         tree = etree.parse(h, etree.XMLParser(recover=True))
